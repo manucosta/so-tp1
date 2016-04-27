f22935dd6c8b3a32a796f45cfb40407378b82fc6
Mario Ezequiel Ginsberg <eginsberg@ws22.labo7>
2016-04-27 17:55:36 -0300

    empece el ejercicio 8, no lo puedo seguir porq voy a clase de algo3 jaja, falta hacer el metodo tick (y ver que funcione). use map, se podra?

--- simusched/sched_rr2.cpp
+++ simusched/sched_rr2.cpp
@@ -8,6 +8,12 @@ using namespace std;
 
 SchedRR2::SchedRR2(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
+	cant_cores = argn[0];
+	for(int i = 1; i <= cant_cores; i++) {
+		quantum_original_cpu.push_back(argn[i]); //lleno el array de cpu_quantum con los quantums de cada core.
+		quantum_restante_cpu.push_back(argn[i]); //le pongo todo el quantum a cada cpu.
+		cant_procesos_cpu[i] = 0;
+	}
 //<<<REMOVE>>>
 //<<<REMOVE END>>>
 }
@@ -20,19 +26,44 @@ SchedRR2::~SchedRR2() {
 void SchedRR2::load(int pid) {
 //<<<REMOVE>>>
 //<<<REMOVE END>>>
+	int indice_cpu_menor = 0;
+	for(int i = 0; i < cant_cores; i++) {
+		if(cant_procesos_cpu[i] < cant_procesos_cpu[indice_cpu_menor]) {
+			indice_cpu_menor = i;
+			break;
+		}
+	}
+	//hasta aca tengo el indice de la cpu que tiene menos procesos
+	cant_procesos_cpu[indice_cpu_menor]++; //ahora esa cpu tiene un proceso mas
+	procesos_por_nucleo.insert( std::pair<int,int>(pid, indice_cpu_menor) ); //agrego la conexion pid-cpu
+	cola_procesos_cpu[indice_cpu_menor].push(pid); //pusheo el proceso a la cola de su cpu.
 }
 
 void SchedRR2::unblock(int pid) {
 //<<<REMOVE>>>
 //<<<REMOVE END>>>
+	int cpu_perteneciente = procesos_por_nucleo.find(pid)->second; //me guardo a que cpu pertenece el proceso con id = pid
+	cola_procesos_cpu[cpu_perteneciente].push(pid); //agrego el proceso a la cola de la cpu perteneciente
 }
 
 int SchedRR2::tick(int cpu, const enum Motivo m) {
 //<<<REMOVE>>>
 //<<<REMOVE END>>>
+	switch(m) {
+		case TICK:
+			//si era el ultimo tick encolarlo y agarrar otro. sino restar quantum.
+		case BLOCK:
+			//poner nuevo pid, recargar quantum
+		case EXIT:
+			cant_procesos_cpu[cpu]--; //ahora hay un proceso menos en la cpu
+			procesos_por_nucleo.erase(current_pid(cpu)); //borro del diccionario el pid del proceso que termino.
+			//falta retornar
+	}
+return 0;
 }
 
 int SchedRR2::next(int cpu) {
 //<<<REMOVE>>>
 //<<<REMOVE END>>>
+return 0;
 }
--- simusched/sched_rr2.h
+++ simusched/sched_rr2.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <queue>
+#include <map>
 #include "basesched.h"
 
 class SchedRR2 : public SchedBase {
@@ -23,6 +24,14 @@ class SchedRR2 : public SchedBase {
 		int ncores;
 //<<<REMOVE END>>>
 		int next(int cpu);
+		
+		
+		int cant_cores; //guarda la cantidad de cores que hay.
+		std::vector< std::queue<int> > cola_procesos_cpu; //indexado por cpu, tiene la cola de procesos para cada cpu
+		std::vector<int> quantum_original_cpu; //quantum original de cada cpu
+		std::vector<int> quantum_restante_cpu; //quantum que le queda al proceso antes de ser desalojado, por cpu
+		std::vector<int> cant_procesos_cpu; //cantidad de procesos que hay en cada cpu (suma los ready+bloqueados+ejecutando)
+		std::map<int,int> procesos_por_nucleo; //un dicc. dado un pid, devuelve la cpu a la que pertenece.
 };
 
 #endif
