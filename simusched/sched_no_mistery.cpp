ab91a7714149655d8e331b2754a3657cd344ca1a
Manuel Jose Joaquin Costa <manuc94@hotmail.com>
2016-05-02 15:49:23 -0300

    sched_no_mistery andando

--- simusched/sched_no_mistery.cpp
+++ simusched/sched_no_mistery.cpp
@@ -8,27 +8,24 @@ using namespace std;
 
 SchedNoMistery::SchedNoMistery(vector<int> argn) {
   // MFQ recibe los quantums por parÃ¡metro
-  def_quantum.push_back(1);
+  std::queue<int> vacio;
   for(int i = 0; i < argn.size(); i++){
-	  std::queue<int> vacio;
 	  def_quantum.push_back(argn[i]);
 	  vq.push_back(vacio);
   }
   cur_pri = 0;
   quantum = def_quantum[cur_pri];
-  n = 0;
+  n = vq.size();
 }
 
 void SchedNoMistery::load(int pid) {
 	vq[0].push(pid);
+  unblock_to.push_back(0);
 }
 
 void SchedNoMistery::unblock(int pid) {
-	for(int i = 0; i < unblock_to.size(); i++){
-		if(unblock_to[i] == pid){
-			// Aca es donde tengo más problemas
-		}
-	}
+  int pri = unblock_to[pid];
+  vq[pri].push(pid);
 }
 
 int SchedNoMistery::tick(int cpu, const enum Motivo m) {
@@ -36,19 +33,20 @@ int SchedNoMistery::tick(int cpu, const enum Motivo m) {
     case TICK:
       if(current_pid(cpu) == IDLE_TASK){  //separamos esto porque no queremos encolar idle
         return next();
-      }else if(quantum == 0) { //uso todo su quantum, sacarlo y poner otro de la cola.
-        if(cur_pri < vq.size()-1){
-			vq[cur_pri+1].push(current_pid(cpu));
+      }else if(quantum == 1) { //uso todo su quantum, sacarlo y poner otro de la cola.
+        if(cur_pri < n-1){
+			   vq[cur_pri+1].push(current_pid(cpu));
         }else{
-			vq[cur_pri].push(current_pid(cpu));
-		}
+			   vq[cur_pri].push(current_pid(cpu));
+		    }
         return next();
       } else { //no uso todo su quantum, restarle 1 al quantum
-        n--;
+        quantum--;
         return current_pid(cpu);
       }
     
     case BLOCK:
+      unblock_to[current_pid(cpu)] = (cur_pri == 0 ? 0 : cur_pri - 1);
       return next();
     
     case EXIT:
@@ -60,8 +58,7 @@ int SchedNoMistery::tick(int cpu, const enum Motivo m) {
 
 int SchedNoMistery::next(void) {
   // Elijo el nuevo pid
-  for(int i = cur_pri; i < vq.size()+cur_pri; i++){
-	  i = i % vq.size();
+  for(int i = 0; i < n; i++){
 	  if(!vq[i].empty()){
 		  quantum = def_quantum[i];
 		  cur_pri = i;
