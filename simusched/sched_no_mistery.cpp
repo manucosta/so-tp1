3fed0d4d34ea55e5319d4fcbdc5e25f05ce2f766
¨camicoy¨ <camicoy94@gmail.com>
2016-05-02 16:21:58 -0300

    codigo comentado

--- simusched/sched_no_mistery.cpp
+++ simusched/sched_no_mistery.cpp
@@ -9,23 +9,23 @@ using namespace std;
 SchedNoMistery::SchedNoMistery(vector<int> argn) {
   // MFQ recibe los quantums por parÃ¡metro
   std::queue<int> vacio;
-  for(int i = 0; i < argn.size(); i++){
-	  def_quantum.push_back(argn[i]);
-	  vq.push_back(vacio);
+  for(int i = 0; i < argn.size(); i++){ //Por default viene el parametro 1 de la primera cola
+	  def_quantum.push_back(argn[i]); //Agrego cada quantum de las colas en en def_quantum
+	  vq.push_back(vacio); //Genero una cola vacia para poder accederla más tarde
   }
-  cur_pri = 0;
+  cur_pri = 0; //la primera prioridad es la de quantum 1
   quantum = def_quantum[cur_pri];
-  n = vq.size();
+  n = vq.size(); //para usar n en los for
 }
 
-void SchedNoMistery::load(int pid) {
-	vq[0].push(pid);
-  unblock_to.push_back(0);
+void SchedNoMistery::load(int pid) { //solo hago load cuando entra por primera vez
+	vq[0].push(pid); //Lo agrego a la primera cola, la de más prioridad
+	unblock_to.push_back(0); //genero el espacio para cuando se bloquee
 }
 
 void SchedNoMistery::unblock(int pid) {
-  int pri = unblock_to[pid];
-  vq[pri].push(pid);
+  int pri = unblock_to[pid]; //Obtengo la prioridad de la pocision de la tarea en unblock_to
+  vq[pri].push(pid); //Lo pongo en la cola
 }
 
 int SchedNoMistery::tick(int cpu, const enum Motivo m) {
@@ -34,9 +34,9 @@ int SchedNoMistery::tick(int cpu, const enum Motivo m) {
       if(current_pid(cpu) == IDLE_TASK){  //separamos esto porque no queremos encolar idle
         return next();
       }else if(quantum == 1) { //uso todo su quantum, sacarlo y poner otro de la cola.
-        if(cur_pri < n-1){
+        if(cur_pri < n-1){ //Como se termino el quantum lo cambio a la siguente cola de prioridad
 			   vq[cur_pri+1].push(current_pid(cpu));
-        }else{
+        }else{ //Como esta en la ultima se mantiene
 			   vq[cur_pri].push(current_pid(cpu));
 		    }
         return next();
@@ -46,7 +46,7 @@ int SchedNoMistery::tick(int cpu, const enum Motivo m) {
       }
     
     case BLOCK:
-      unblock_to[current_pid(cpu)] = (cur_pri == 0 ? 0 : cur_pri - 1);
+      unblock_to[current_pid(cpu)] = (cur_pri == 0 ? 0 : cur_pri - 1); // agrego en la posición del la tarea, segun su pid, la prioridad que le toca
       return next();
     
     case EXIT:
@@ -58,10 +58,10 @@ int SchedNoMistery::tick(int cpu, const enum Motivo m) {
 
 int SchedNoMistery::next(void) {
   // Elijo el nuevo pid
-  for(int i = 0; i < n; i++){
-	  if(!vq[i].empty()){
-		  quantum = def_quantum[i];
-		  cur_pri = i;
+  for(int i = 0; i < n; i++){ //Siempre reviso desde la de prioridad más alta hasta la de más baja
+	  if(!vq[i].empty()){ //Cuando encuentro una cola con tareas busca la primera de la cola
+		  quantum = def_quantum[i]; //renuevo el quantum
+		  cur_pri = i; //actualizo la prioridad
 		  int pid = vq[i].front();
 		  vq[i].pop();
 		  return pid;
